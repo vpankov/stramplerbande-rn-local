ddf05ab91112f4ab0b94cef9e4705bd09b1766f1
Vitaly Pankov <v.pankov>
2019-12-29 19:28:36 +0300

    sey sbdemo api

--- src/http/apiClient.js
+++ src/http/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as store from '../store'
 
 const _axios = axios.create({
-    baseURL: 'https://strampler-bande.de',
+    baseURL: 'http://sbdemo.theleanapps.com/',
 });
 
 _axios.interceptors.request.use(
