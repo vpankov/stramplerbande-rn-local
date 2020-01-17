e1fd5194c6adbc8e49e9954b12762f882f31cab9
Игорь Лученков <igorlucenkov@Igors-MacBook-Pro.local>
2020-01-17 17:21:22 +0300

    production_fb: remove unnecessary files

--- src/http/apiClient.js
+++ src/http/apiClient.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import * as store from '../store';
 
 // export const apiURL = 'https://safe-to-connect.com:8091/api';
 // export const apiURL = 'https://stramplerbande.org:8091/api';
@@ -7,28 +6,7 @@ export const apiURL = 'http://sbtest.theleanapps.com:8091/api';
 // export const apiURL = 'http://sbdemo.theleanapps.com:8091/api';
 
 const _axios = axios.create({
-    baseURL: apiURL,
+  baseURL: apiURL,
 });
 
-_axios.interceptors.request.use(
-    async (request) => {
-        if (request.url !== '/loginapi.php') {
-            const { username, password } = await store.getCredentials()
-            request.data = {
-                ...request.data,
-                username,
-                password,
-            }
-        }
-
-        return request
-    },
-    (error) => { return Promise.reject(error); }
-);
-
-_axios.interceptors.response.use(
-    (response) => { return response; },
-    (error) => { Promise.reject(error) }
-);
-
-export default _axios
+export default _axios;
