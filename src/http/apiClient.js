c1f5a5baf05b61e1d3dbf0cc1c6ae620c0f0a0ab
Vitaly Pankov <v.pankov@nicecode.biz>
2020-01-20 22:16:45 +0300

    incremented build versions

--- src/http/apiClient.js
+++ src/http/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// export const apiURL = 'https://safe-to-connect.com:8091/api';
+export const apiURL = 'https://safe-to-connect.com:8091/api';
 // export const apiURL = 'https://stramplerbande.org:8091/api';
-export const apiURL = 'http://sbtest.theleanapps.com:8091/api';
+// export const apiURL = 'http://sbtest.theleanapps.com:8091/api';
 // export const apiURL = 'http://sbdemo.theleanapps.com:8091/api';
 
 const _axios = axios.create({
